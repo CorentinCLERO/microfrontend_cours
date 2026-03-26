2182922ae86053174fa3ba97fd1d87b37d7c8f2c
CorentinCLERO <corentinclero@hotmail.com>
2026-03-26 12:24:47 +0100

    Make navbar and lobby to communicate

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -6,8 +6,11 @@ function Navbar() {
   const [notifications, setNotifications] = useState(0);
 
   useEffect(() => {
-    // TODO: quand un joueur rejoint une partie, incrementer le badge notifications
-    // Penser au cleanup React
+    const unsubscribe = eventBus.on('game:joined', () => {
+      setNotifications(prev => prev + 1);
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
